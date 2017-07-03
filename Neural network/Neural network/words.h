e4ea277b251bf1636d195a5f8c060021a874abfb
will2216 <williamjensenbc@gmail.com>
2017-07-04 01:26:44 +0200

    Add files via upload

---
+++ Neural network/Neural network/words.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef WORDS_H_
+#define WORDS_H_
+
+
+class words {
+public:
+	words();
+	std::string ** Lang;
+
+	//void SETLANGUAGES(std::string accesspoint, int number);
+	double char_to_int(char input);
+};
+
+
+
+#endif
